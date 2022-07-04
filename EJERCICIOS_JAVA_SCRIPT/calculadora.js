ee7c1ef2cf5cb22f6c3e021b263acf05869d6a53
fidelmr <fde_los_riscos@uoc.edu>
2022-07-04 21:58:26 +0200

    practica-calculadora

---
+++ EJERCICIOS_JAVA_SCRIPT/calculadora.js
@@ -0,0 +1,17 @@
+let mood = 'bn';
+
+
+
+switch (mood) {
+    case 'happy':
+        console.log('😀')
+        break;
+
+
+    case 'sad':
+        console.log('😥')
+        break;
+        
+    case 'default';
+        console.log('🤡')
+}
